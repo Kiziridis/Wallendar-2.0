668b1b64a533f1a0b2da75f0997bb82a03b536fe
Matthaios Giannopapas <153185447+mgiannopa@users.noreply.github.com>
2024-11-28 22:09:28 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -4,18 +4,11 @@ const got = require('got');
 
 const app = require('../index');
 
-test.before(async (t) => {
-	t.context.server = http.createServer(app);
-	t.context.prefixUrl = await listen(t.context.server);
-	t.context.got = got.extend({ http2: true, throwHttpErrors: false, responseType: "json", prefixUrl: t.context.prefixUrl });
+test("A test that passes", (t) => {
+	t.pass();
 });
 
-test.after.always((t) => {
-	t.context.server.close();
+test("A test that fails", (t) => {
+	t.fail();
 });
 
-test("GET / returns correct response and status code", async (t) => {
-	const { body, statusCode } = await t.context.got("api");
-	t.is(body.message, "It works!");
-	t.is(statusCode, 200);
-});
