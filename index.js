1ba686cd221609e67d0c48be3119f0bf5cf2834f
tmpillas <74024931+tmpillas@users.noreply.github.com>
2024-11-30 14:00:46 +0200

    Add files via upload
    updated tests, index.js and package.json

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 var path = require('path');
 var http = require('http');
+//var express = require('express');
 
 var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
+
+const { NODE_ENV, PORT } = process.env;
 
 // swaggerRouter configuration
 var options = {
@@ -17,8 +20,49 @@ var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/open
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-});
+// http.createServer(app).listen(serverPort, function () {
+//     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+//     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+// });
+const server = http.Server(app);
+
+if (NODE_ENV !== "test") {
+    server.listen(serverPort, () => {
+    console.log(`>>> Live at http://localhost:${serverPort}`);
+    console.log(`Swagger-ui is available on http://localhost:${serverPort}/docs`);
+ });
+}
+
+module.exports = app;
+
+
+
+
+
+
+// //TIFOMI GOAT
+// 'use strict';
+
+// var path = require('path');
+// var http = require('http');
+// //var express = require('express');
+
+// var oas3Tools = require('oas3-tools');
+// var serverPort = 8080;
+
+// // swaggerRouter configuration
+// var options = {
+//     routing: {
+//         controllers: path.join(__dirname, './controllers')
+//     },
+// };
+
+// var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+// var app = expressAppConfig.getApp();
+
+// // Initialize the Swagger middleware
+// http.createServer(app).listen(serverPort, function () {
+//     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+//     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+// });
 
