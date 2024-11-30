ba9a4a0bcad835412433dd8531cf092004db8a0e
Matthaios Giannopapas <153185447+mgiannopa@users.noreply.github.com>
2024-11-30 12:10:46 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -18,8 +18,8 @@ test.after.always((t) => {
 test("A test that passes", (t) => {
 	t.pass();
 });
-test("GET /api returns correct response and status code", async (t) => {
-	const { body, statusCode } = await t.context.got("api");
+test("GET /users returns correct response and status code", async (t) => {
+	const { body, statusCode } = await t.context.got("users");
 	t.is(body.message, "It works!");
 	t.is(statusCode, 200);
 });
