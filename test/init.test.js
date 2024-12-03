6b1f61a9c0180dcc574437a78e489bf2627ec6fe
dimitris kiziridis <kiziridis@ece.auth.gr>
2024-12-03 16:22:45 +0200

    1st test complete and ci-cd works (i think)

--- test/init.test.js
+++ test/init.test.js
@@ -1,23 +1,34 @@
-
+const http = require('http');
 const request = require('supertest');
 const app = require('../index'); // Ensure this path is correct
+const test = require('ava');
 
-describe('GET /users', function() {
-    it('should return user kizi if exists', function(done) {
-        request(app)
-            .get('/users')
-            .query({ username: 'kizi' })
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-                const users = res.body;
-                const userExists = users.some(user => user.username === 'kizi');
-                if (userExists) {
-                    done();
-                } else {
-                    done(new Error('User kizi not found'));
-                }
-            });
+let server;
+
+test.before(async t => {
+    console.log('Starting server... :)');
+    server = http.createServer(app);
+    await new Promise(resolve => {
+        server.listen(0, () => {
+            const { port } = server.address();
+            t.context.got = require('got').extend({ responseType: 'json', prefixUrl: `http://localhost:${port}` });
+            console.log(`Server started on port ${port}`);
+            resolve();
+        });
     });
+});
+
+test.after.always(t => {
+    console.log('Closing server...');
+    server.close(() => {
+        console.log('Server closed');
+    });
+});
+
+test('GET /users', async t => {
+    const response = await t.context.got.get('users', { searchParams: { username: 'kizi' } });
+    t.is(response.statusCode, 200);
+    const users = response.body;
+    const userExists = users.some(user => user.username === 'kizi');
+    t.true(userExists, 'User kizi not found');
 });
