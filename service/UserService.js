631637e8371baf929ad76d7880e2e8de77bf4b42
dimitris kiziridis <kiziridis@ece.auth.gr>
2024-12-04 17:28:00 +0200

    .

--- service/UserService.js
+++ service/UserService.js
@@ -1,25 +1,32 @@
 'use strict';
 
-let dummyUsers = [
-  {
-    userId: 1,
-    username: 'john-doe',
-    email_address: 'john@example.com',
-    password: 'password',
-    preferred_language: 'Greek',
-  },
-  {
-    userId: 2,
-    username: 'kizi',
-    email_address: 'kizi@example.com',
-    password: 'password',
-    preferred_language: 'Greek',
-  },
-];
-
-exports.searchUsers = function (username) {
-  return new Promise(function (resolve, reject) {
-    const filteredUsers = dummyUsers.filter((user) =>
+/**
+ * Search for other users.
+ * FR5: The user must be able to search for other users by username. 
+ *
+ * username String username provided for the search
+ * returns Users
+ **/
+exports.searchUsers = function(username) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [
+      {
+        password: 'password',
+        email_address: 'john@example.com',
+        userId: 1,
+        preferred_language: 'Greek',
+        username: 'john-doe'
+      },
+      {
+        password: 'password',
+        email_address: 'kizi@example.com',
+        userId: 2,
+        preferred_language: 'Greek',
+        username: 'kizi'
+      }
+    ];
+    const filteredUsers = examples['application/json'].filter((user) =>
       user.username.includes(username)
     );
     if (filteredUsers.length > 0) {
