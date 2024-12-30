af0062aec74d933a946f6a5c5a77c10dbc0ef169
tmpillas <tmpillas@ece.auth.gr>
2024-12-30 15:33:25 +0200

    all

--- controllers/FriendRequest.js
+++ controllers/FriendRequest.js
@@ -1,57 +1,63 @@
 'use strict';
 
-// Importing required modules
+// Import the utils module for writing JSON responses
 var utils = require('../utils/writer.js');
+
+// Import the FriendRequest service module
 var FriendRequest = require('../service/FriendRequestService');
 
-// Function to accept or decline a friend request
-module.exports.acceptDeclineFriendRequest = function acceptDeclineFriendRequest (_req, res, _next, body, friendRequestId) {
+// Function to handle accepting or declining a friend request
+module.exports.acceptDeclineFriendRequest = function acceptDeclineFriendRequest (req, res, next, body, friendRequestId) {
+  // Call the acceptDeclineFriendRequest function from the FriendRequest service
   FriendRequest.acceptDeclineFriendRequest(body, friendRequestId)
     .then(function (response) {
-      // Sending success response
+      // If the promise resolves, write the JSON response
       utils.writeJson(res, response);
     })
     .catch(function (response) {
-      // Sending error response
+      // If the promise rejects, write the JSON response
       utils.writeJson(res, response);
     });
 };
 
-// Function to cancel a friend request
-module.exports.cancelFriendRequest = function cancelFriendRequest (_req, res, _next, friendRequestId) {
+// Function to handle canceling a friend request
+module.exports.cancelFriendRequest = function cancelFriendRequest (req, res, next, friendRequestId) {
+  // Call the cancelFriendRequest function from the FriendRequest service
   FriendRequest.cancelFriendRequest(friendRequestId)
     .then(function (response) {
-      // Sending success response
+      // If the promise resolves, write the JSON response
       utils.writeJson(res, response);
     })
     .catch(function (response) {
-      // Sending error response
+      // If the promise rejects, write the JSON response
       utils.writeJson(res, response);
     });
 };
 
-// Function to receive a friend request
-module.exports.receiveFriendRequest = function receiveFriendRequest (_req, res, _next, senderId, friendRequestId) {
+// Function to handle receiving a friend request
+module.exports.receiveFriendRequest = function receiveFriendRequest (req, res, next, senderId, friendRequestId) {
+  // Call the receiveFriendRequest function from the FriendRequest service
   FriendRequest.receiveFriendRequest(senderId, friendRequestId)
     .then(function (response) {
-      // Sending success response
+      // If the promise resolves, write the JSON response
       utils.writeJson(res, response);
     })
     .catch(function (response) {
-      // Sending error response
+      // If the promise rejects, write the JSON response
       utils.writeJson(res, response);
     });
 };
 
-// Function to send a friend request
-module.exports.sendFriendRequest = function sendFriendRequest (_req, res, _next, body, receiverId) {
+// Function to handle sending a friend request
+module.exports.sendFriendRequest = function sendFriendRequest (req, res, next, body, receiverId) {
+  // Call the sendFriendRequest function from the FriendRequest service
   FriendRequest.sendFriendRequest(body, receiverId)
     .then(function (response) {
-      // Sending success response
+      // If the promise resolves, write the JSON response
       utils.writeJson(res, response);
     })
     .catch(function (response) {
-      // Sending error response
+      // If the promise rejects, write the JSON response
       utils.writeJson(res, response);
     });
 };
