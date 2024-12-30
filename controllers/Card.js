ea94285d5bf6dad88ed8e4d3cb42bd3882b5fd48
mgiannopa <mgiannopa@ece.auth.gr>
2024-12-30 12:47:23 +0200

    comments added

--- controllers/Card.js
+++ controllers/Card.js
@@ -1,14 +1,21 @@
 'use strict';
 
+// Import the utils module for writing JSON responses
 var utils = require('../utils/writer.js');
+
+// Import the Card service module
 var Card = require('../service/CardService');
 
+// Function to handle selecting a card
 module.exports.selectCard = function selectCard (req, res, next, walletId, cardNumber) {
+  // Call the selectCard function from the Card service
   Card.selectCard(walletId, cardNumber)
     .then(function (response) {
+      // If the promise resolves, write the JSON response with the response code
       utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
+      // If the promise rejects, write the JSON response with the response code
       utils.writeJson(res, response, response.code);
     });
 };
